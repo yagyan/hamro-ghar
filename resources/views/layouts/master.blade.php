7b794152b8a1d0c2cad61cf616668283c7ba2252
yagyan <yagyanmunankarmi@gmail.com>
2020-05-27 10:24:36 +0545

    added logo in maste.blade.php

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -59,9 +59,9 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <!-- Brand Logo -->
 
     <a href="/" class="brand-link">
-    <!--  <img src="/images/deafult.jpg" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
-           style="opacity: .8"> -->
-      <span class="brand-text font-weight-light">Hamro Ghar</span>
+      <img src="img/logo.png" alt="AdminLTE Logo" 
+           style="opacity: .8"> 
+      
     </a>
 
     <!-- Sidebar -->

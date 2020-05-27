e70a1b21e42edd025df60599cf136984667464d7
yagyan <yagyanmunankarmi@gmail.com>
2020-05-27 21:02:05 +0545

    change route to index after login

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -22,6 +22,7 @@ class RouteServiceProvider extends ServiceProvider
      * @var string
      */
     public const HOME = '/home';
+    public const index= '/';
 
     /**
      * Define your route model bindings, pattern filters, etc.

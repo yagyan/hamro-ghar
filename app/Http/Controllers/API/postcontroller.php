dc92b7c777443ea55c3e28b4175ab6ab2927f7d3
yagyan <yagyanmunankarmi@gmail.com>
2020-05-29 21:06:20 +0545

    modification

--- app/Http/Controllers/API/postcontroller.php
+++ app/Http/Controllers/API/postcontroller.php
@@ -36,6 +36,16 @@ class postcontroller extends Controller
         ->where('posts.id',$id)->get();
         return $post;    
     }
+
+    public function searchpost($type, $bed, $bath)
+    {   
+        $post=DB::table('posts')
+        ->join('propertytypes','posts.propertytype_id','=','propertytypes.id')
+        ->select('posts.*','propertytypes.name as pname')
+        ->where('propertytypes.id',$type)->where('posts.bed',$bed)->where('posts.bathroom',$bath)->get();
+        return $post;    
+    }
+
     public function index()
     {
         //

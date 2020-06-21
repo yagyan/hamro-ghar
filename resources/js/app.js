c777407809bb1a20a7db502a60bd03b5ea6082b2
yagyan <yagyanmunankarmi@gmail.com>
2020-06-21 10:12:15 +0545

    formatting

--- resources/js/app.js
+++ resources/js/app.js
@@ -21,50 +21,121 @@ Vue.use(VueRouter);
 
 //sweet alert 2
 import Swal from 'sweetalert2';
-window.Swal=Swal;
+window.Swal = Swal;
 
 //vue progressbar
 
 import VueProgressBar from 'vue-progressbar';
 const options = {
-  color: '#228B22',
-  failedColor: '#874b4b',
-  thickness: '3px',
-  transition: {
-    speed: '0.2s',
-    opacity: '0.6s',
-    termination: 300
-  },
-  autoRevert: true,
-  location: 'top',
-  inverse: false
+    color: '#228B22',
+    failedColor: '#874b4b',
+    thickness: '3px',
+    transition: {
+        speed: '0.2s',
+        opacity: '0.6s',
+        termination: 300
+    },
+    autoRevert: true,
+    location: 'top',
+    inverse: false
 };
 
 Vue.use(VueProgressBar, options);
 
 
-let routes=[
-{path:'/state', component:require('./components/state.vue').default},
-{path:'/district', component:require('./components/district.vue').default},
-{path:'/municipality', component:require('./components/municipality.vue').default},
-{path:'/ward', component:require('./components/ward.vue').default},
-{path:'/propertytype', component:require('./components/propertytype.vue').default},
-{path:'/userprofile', component:require('./components/userprofile.vue').default},
-{path:'/usertype', component:require('./components/usertype.vue').default,name:'usertype'},
-{path:'/usermanagement', component:require('./components/usermanagement.vue').default},
-{path:'/paddress', component:require('./components/paddress.vue').default},
-{path:'/post', component:require('./components/post.vue').default},
-{path:'/adminpost', component:require('./components/adminpost.vue').default},
-{ path: '/',component: require('./components/index.vue').default},
-{ path: '/registerform',component: require('./components/register.vue').default},
-{ path: '/allproperty',component: require('./components/allproperty.vue').default,name:'allproperty'},
-{ path: '/about',component: require('./components/about.vue').default},
-{ path: '/contact',component: require('./components/contact.vue').default},
-{ path: '/newpost',component: require('./components/newpost.vue').default},
-{ path: '/yourpost',component: require('./components/yourpost.vue').default, name:'yourpost'},
-{ path: '/singlepost',component: require('./components/singlepost.vue').default, name:'singlepost',props: true},
-{ path: '/editpost',component: require('./components/editpost.vue').default, name:'',props: true},
-{path: '/search',component: require('./components/search.vue').default, name:'search',props: true},
+let routes = [{
+        path: '/state',
+        component: require('./components/state.vue').default
+    },
+    {
+        path: '/district',
+        component: require('./components/district.vue').default
+    },
+    {
+        path: '/municipality',
+        component: require('./components/municipality.vue').default
+    },
+    {
+        path: '/ward',
+        component: require('./components/ward.vue').default
+    },
+    {
+        path: '/propertytype',
+        component: require('./components/propertytype.vue').default
+    },
+    {
+        path: '/userprofile',
+        component: require('./components/userprofile.vue').default
+    },
+    {
+        path: '/usertype',
+        component: require('./components/usertype.vue').default,
+        name: 'usertype'
+    },
+    {
+        path: '/usermanagement',
+        component: require('./components/usermanagement.vue').default
+    },
+    {
+        path: '/paddress',
+        component: require('./components/paddress.vue').default
+    },
+    {
+        path: '/post',
+        component: require('./components/post.vue').default
+    },
+    {
+        path: '/adminpost',
+        component: require('./components/adminpost.vue').default
+    },
+    {
+        path: '/',
+        component: require('./components/index.vue').default
+    },
+    {
+        path: '/registerform',
+        component: require('./components/register.vue').default
+    },
+    {
+        path: '/allproperty',
+        component: require('./components/allproperty.vue').default,
+        name: 'allproperty'
+    },
+    {
+        path: '/about',
+        component: require('./components/about.vue').default
+    },
+    {
+        path: '/contact',
+        component: require('./components/contact.vue').default
+    },
+    {
+        path: '/newpost',
+        component: require('./components/newpost.vue').default
+    },
+    {
+        path: '/yourpost',
+        component: require('./components/yourpost.vue').default,
+        name: 'yourpost'
+    },
+    {
+        path: '/singlepost',
+        component: require('./components/singlepost.vue').default,
+        name: 'singlepost',
+        props: true
+    },
+    {
+        path: '/editpost',
+        component: require('./components/editpost.vue').default,
+        name: '',
+        props: true
+    },
+    {
+        path: '/search',
+        component: require('./components/search.vue').default,
+        name: 'search',
+        props: true
+    },
 
 
 
@@ -72,15 +143,19 @@ let routes=[
 ];
 
 //Vue Router Register
-const router= new VueRouter({
-  mode:'history',
-	routes:routes,
+const router = new VueRouter({
+    mode: 'history',
+    routes: routes,
 });
 
 
 //v form
-import { Form, HasError, AlertError } from 'vform';
-window.Form=Form;
+import {
+    Form,
+    HasError,
+    AlertError
+} from 'vform';
+window.Form = Form;
 Vue.component(HasError.name, HasError);
 Vue.component(AlertError.name, AlertError);
 
@@ -109,10 +184,10 @@ Vue.component('slider.vue', require('./components/ExampleComponent.vue').default
  */
 
 const app = new Vue({
-    store, 
+    store,
     el: '#app',
     router,
-    
 
-   
+
+
 });

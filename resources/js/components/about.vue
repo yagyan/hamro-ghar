9f3b80c7faa17680c8390e41d1993e2cfaf341d8
yagyan <yagyanmunankarmi@gmail.com>
2020-08-24 15:22:46 +0545

    check

--- resources/js/components/about.vue
+++ resources/js/components/about.vue
@@ -1,6 +1,6 @@
 <template>
     <div style="padding-top:150px">
-         
+
         <agents></agents>
         <testimonial></testimonial>
     </div>
@@ -10,8 +10,8 @@
 import test from "../components/testimonial.vue"
 import agents from "../components/agents.vue"
     export default {
-        components:{   
-            'testimonial': test,            
+        components:{
+            'testimonial': test,
             'agents': agents,
         },
         mounted() {

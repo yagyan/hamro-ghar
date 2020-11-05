9c3b0a01b93c1d2921cc0f7665216acb7044710f
yagyan <yagyanmunankarmi@gmail.com>
2020-11-05 07:43:02 +0545

    minor changes

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -189,28 +189,6 @@
         </div>
 
 
-
-
-        <!-- contact_action_area  -->
-        <!-- <div class="contact_action_area">
-            <div class="container">
-                <div class="row">
-                    <div class="col-xl-7">
-                        <div class="action_heading">
-                            <h3>Add your property for sale</h3>
-                        </div>
-                    </div>
-                    <div class="col-xl-5">
-                        <div class="call_add_action">
-                            <span>+10 637 367 4567</span>
-                            <a href="#" class="boxed-btn3-line">Add Property</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>-->
-        <!-- /contact_action_area  -->
-
         <!-- footer start -->
         <footer class="footer">
             <div class="footer_top">
